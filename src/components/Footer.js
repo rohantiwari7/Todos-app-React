62a60c482cc5faaacd1876175e49e824fb78083d
rohan <rohantiwari1024@gmail.com>
2023-07-16 02:10:56 +0530

    bug fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='bg-dark text-light text-center py-2'>Copyrkght &copy; Rohan Tiwari 2023</div>
+    <div className='bg-dark text-light text-center py-2'>Copyright &copy; Rohan Tiwari 2023</div>
   )
 }
